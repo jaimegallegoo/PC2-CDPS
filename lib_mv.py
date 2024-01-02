a570322d90e9273a39e9ac0ba208b0a4158f7f84
jaimegallegoo <jaimegallegochillon@gmail.com>
2024-01-02 10:30:00 +0100

    corregir error

--- lib_mv.py
+++ lib_mv.py
@@ -11,7 +11,7 @@ log = logging.getLogger('auto_p2')
 # Despliegue de la aplicación en máquina virtual pesada
 def mv_pesada (puerto):
   log.debug("mv_pesada ")
-  subprocess.call(['git', 'clone', 'git clone https://github.com/CDPS-ETSIT/practica_creativa2.git'])
+  subprocess.call(['git', 'clone', 'https://github.com/CDPS-ETSIT/practica_creativa2.git'])
   subprocess.call(['cd', 'practica_creativa2/bookinfo/src/productpage'])
   subprocess.call(['pip3', 'install', '-r', 'requirements.txt'])
   subprocess.call(['python3', 'productpage_monolith.py', f'{puerto}'])
