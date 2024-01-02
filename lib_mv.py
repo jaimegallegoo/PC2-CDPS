afcb56e68b9f3d827c07f80ca826d054d02e0d49
Marta Volpini López <marta.volpini@alumnos.upm.es>
2024-01-02 10:50:32 +0100

    prueba oschdir

--- lib_mv.py
+++ lib_mv.py
@@ -4,7 +4,7 @@
 # Jaime Gallego Chillón
 # Marta Volpini López
 
-import logging, subprocess
+import logging, subprocess, os
 
 log = logging.getLogger('auto_p2')
 
@@ -12,6 +12,8 @@ log = logging.getLogger('auto_p2')
 def mv_pesada (puerto):
   log.debug("mv_pesada ")
   subprocess.call(['git', 'clone', 'https://github.com/CDPS-ETSIT/practica_creativa2.git'])
+  # Cambiar al directorio clonado
+  os.chdir('practica_creativa2/bookinfo/src/productpage')
   subprocess.call(['cd', 'practica_creativa2/bookinfo/src/productpage'])
   subprocess.call(['pip3', 'install', '-r', 'requirements.txt'])
   subprocess.call(['python3', 'productpage_monolith.py', f'{puerto}'])
