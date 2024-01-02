0cfd00f43acfac2c624a6319cba33e90e929f71e
jaimegallegoo <jaimegallegochillon@gmail.com>
2024-01-02 14:25:19 +0100

    Poner la creación de imágenes de la parte 3

--- lib_mv.py
+++ lib_mv.py
@@ -21,3 +21,13 @@ def mv_docker ():
   log.debug("mv_docker ")
   subprocess.call(['sudo', 'docker', 'build', '-t', 'g27/product-page', '.'])
   subprocess.call(['sudo', 'docker', 'run', '--name', 'g27-product-page', '-p', '9080:9080', '-d', 'g27/product-page'])
+
+# Despliegue de la aplicación mediante Docker-Compose
+def mv_docker_compose (version):
+  log.debug("mv_docker_compose ")
+  subprocess.call(['sudo', 'docker', 'build', '-t', 'g27/product-page', '/ProductPage'])
+  subprocess.call(['sudo', 'docker', 'build', '-t', 'g27/details', '/Details'])
+  #V1 subprocess.call(['sudo', 'docker', 'build', '--build-arg service_version=v1', '--build-arg enable_ratings=false', '-t', 'g27/reviews-v1', '/Reviews'])
+  #V2 subprocess.call(['sudo', 'docker', 'build', '--build-arg service_version=v2', '--build-arg enable_ratings=true', '--build-arg star_color=black', '-t', 'g27/reviews-v2', '/Reviews'])
+  #V3 subprocess.call(['sudo', 'docker', 'build', '--build-arg service_version=v3', '--build-arg enable_ratings=true', '--build-arg star_color=red', '-t', 'g27/reviews-v3', '/Reviews'])
+  subprocess.call(['sudo', 'docker', 'build', '-t', 'g27/ratings', '/Ratings'])
