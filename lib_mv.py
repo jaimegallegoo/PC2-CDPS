457a9167e4df7cd4d8e347176ca75fd096143f32
jaimegallegoo <jaimegallegochillon@gmail.com>
2024-01-08 14:32:59 +0100

    ya

--- lib_mv.py
+++ lib_mv.py
@@ -41,7 +41,7 @@ def mv_docker_compose (version, ratings, star):
   #Crear la imagen de Reviews
   log.debug("CONSTRUIR REVIEWS")
   os.chdir('practica_creativa2/bookinfo/src/reviews')
-  subprocess.call(['sudo', 'docker', 'run', '--rm', '-u', 'root', '-v', f'{raiz}:/home/gradle/project', '-w', '/home/gradle/project', 'gradle:4.8.1', 'gradle', 'clean', 'build'])
+  subprocess.call(['sudo', 'docker', 'run', '--rm', '-u', 'root', '-v', '/home/gradle/project', '-w', '/home/gradle/project', 'gradle:4.8.1', 'gradle', 'clean', 'build'])
   subprocess.call(['sudo', 'docker', 'build', '--build-arg', 'service_version=v1', '--build-arg', 'enable_ratings=false', '-t', 'g27/reviews-v1', './reviews-wlpcfg'])
   subprocess.call(['sudo', 'docker', 'build', '--build-arg', 'service_version=v2', '--build-arg', 'enable_ratings=true', '--build-arg', 'star_color=black', '-t', 'g27/reviews-v2', './reviews-wlpcfg'])
   subprocess.call(['sudo', 'docker', 'build', '--build-arg', 'service_version=v3', '--build-arg', 'enable_ratings=true', '--build-arg', 'star_color=red', '-t', 'g27/reviews-v3', './reviews-wlpcfg'])
@@ -51,32 +51,32 @@ def mv_docker_compose (version, ratings, star):
   #Crear el contenido del fichero docker-compose.yaml
   log.debug("CONSTRUIR DOCKER_COMPOSE")
   contenido_docker_compose = f"""
-      version: "3.3"
+      version: '3.3'
 
       services:
         g27-productpage:
-          image: "g27/product-page"
+          image: g27/product-page:latest
           ports:
-            - "9080"
+            - 9080:9080
 
         g27-details:
-          image: "g27/details"
+          image: g27/details:latest
           ports:
-            - "9080"
+            - 9080
 
         g27-reviews:
-          image: "g27/reviews-{version}"
+          image: g27/reviews-{version}:latest
           ports:
-            - "9080"
+            - 9080
           environment:
             - SERVICE_VERSION={version}
             - ENABLE_RATINGS={ratings}
             - STAR_COLOR={star}
 
         g27-ratings:
-          image: "g27/ratings"
+          image: g27/ratings:latest
           ports:
-            - "9080"
+            - 9080
       """
   #Escribir el contenido en el fichero docker-compose.yaml
   with open('docker-compose.yaml', 'w') as file:
