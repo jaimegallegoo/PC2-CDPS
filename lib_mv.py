e4c73b3c5c8cf1810e006f4382a2b90263356253
jaimegallegoo <jaimegallegochillon@gmail.com>
2024-01-19 13:58:27 +0100

    prueba

--- lib_mv.py
+++ lib_mv.py
@@ -11,8 +11,8 @@ log = logging.getLogger('auto_p2')
 # Despliegue de la aplicación en máquina virtual pesada
 def mv_pesada (puerto):
   log.debug("mv_pesada ")
-  subprocess.call(['git', 'clone', 'https://github.com/CDPS-ETSIT/practica_creativa2.git'])
-  subprocess.run(['find', './', '-type', 'f', '-exec', 'sed', '-i', f's/Simple Bookstore App/GRUPO27/g', '{{}}', '\;'])
+  #subprocess.call(['git', 'clone', 'https://github.com/CDPS-ETSIT/practica_creativa2.git'])
+  #subprocess.run(['find', './', '-type', 'f', '-exec', 'sed', '-i', f's/Simple Bookstore App/GRUPO27/g', '{{}}', '\;'])
   os.chdir('practica_creativa2/bookinfo/src/productpage')
   subprocess.call(['pip3', 'install', '-r', 'requirements.txt'])
   subprocess.call(['python3', 'productpage_monolith.py', f'{puerto}'])
